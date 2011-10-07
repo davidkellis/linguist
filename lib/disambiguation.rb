a846a298e64806deb20c956290b4f71bc980fcd0
David Ellis <davidkellis@gmail.com>
2011-10-06 19:47:53 -0500

    Moved all of the disambiguation rule processing so that it occurs before parse tree generation instead of during tree generation. Further significant speedups!

--- lib/disambiguation.rb
+++ lib/disambiguation.rb
@@ -17,55 +17,6 @@ module Linguist
         @avoided_productions = {}
       end
 
-      def tree_obeys_disambiguation_rules?(token_stream, tree_root_node)
-        tree_root_node.descendants.all? { |tree_node| subtree_obeys_disambiguation_rules?(token_stream, tree_node) }
-      end
-
-      def subtree_obeys_disambiguation_rules?(token_stream, parent_node)
-        # subtree_obeys_priority_rules?(parent_node) && 
-        # subtree_obeys_associativity_rules?(parent_node) &&
-        subtree_obeys_reject_rules?(token_stream, parent_node) &&
-        subtree_obeys_follow_restrictions?(token_stream, parent_node)
-      end
-      
-      # def subtree_obeys_priority_rules?(tree_root_node)
-      #   priority_tree.is_parse_tree_valid?(tree_root_node)
-      # end
-      
-      # def subtree_obeys_associativity_rules?(tree_root_node)
-      #   if tree_root_node.non_terminal?
-      #     if associativity_rules.include?(tree_root_node.production)
-      #       associativity_rule = associativity_rules[tree_root_node.production]
-      #       associativity_rule.is_parse_tree_valid?(tree_root_node)
-      #     else
-      #       true    # there is no associativity rule that applies to the production from which tree_root_node was derived
-      #     end
-      #   else
-      #     true      # the tree_root_node represents a terminal symbol, so it is a valid parse tree (i.e. it is a leaf node)
-      #   end
-      # end
-
-      # Rejects
-      # http://homepages.cwi.nl/~daybuild/daily-books/syntax/2-sdf/sdf.html#section.disambrejects
-      # Given non-terminal N and regular expression R, we tell the parser to reject
-      # any derivation N => S where S is a string that is derivable from the regular expression R.
-      # In other words, if S matches the regular expression R, then we prune the tree in which the derivation
-      # N => S exists.
-      def subtree_obeys_reject_rules?(token_stream, parent_node)
-        if parent_node.non_terminal?
-          derivation_string = token_stream[parent_node.start_index...parent_node.end_index].join()
-          (reject_rules[parent_node.production.non_terminal] || []).none? do |rejection_pattern|
-            if rejection_pattern.is_a? Regexp
-              derivation_string =~ rejection_pattern
-            else
-              derivation_string == rejection_pattern
-            end
-          end
-        else
-          true
-        end
-      end
-
       # Follow Restrictions
       # http://homepages.cwi.nl/~daybuild/daily-books/syntax/2-sdf/sdf.html#section.restrictions
       # Given non-terminal N and regular expression R, we tell the parser to reject
@@ -73,7 +24,11 @@ module Linguist
       # by X, a string that is derivable from the regular expression R.
       # In other words, if N derives string S, and S is followed by substring X, and X matches the regular expression R, 
       # then we prune the tree in which the derivation N => S exists.
-      def subtree_obeys_follow_restrictions?(token_stream, parent_node)
+      def select_nodes_obeying_follow_restrictions(token_stream, non_terminal_nodes)
+        non_terminal_nodes.select { |node| node_obeys_follow_restrictions?(token_stream, node) }
+      end
+
+      def node_obeys_follow_restrictions?(token_stream, parent_node)
         if parent_node.non_terminal?
           follow_string_scanner = StringScanner.new(token_stream[parent_node.end_index..-1].join())
           derivation_string = token_stream[parent_node.start_index...parent_node.end_index].join()
@@ -194,6 +149,31 @@ module Linguist
         end
       end
 
+      # Rejects
+      # http://homepages.cwi.nl/~daybuild/daily-books/syntax/2-sdf/sdf.html#section.disambrejects
+      # Given non-terminal N and regular expression R, we tell the parser to reject
+      # any derivation N => S where S is a string that is derivable from the regular expression R.
+      # In other words, if S matches the regular expression R, then we prune the tree in which the derivation
+      # N => S exists.
+      def select_nodes_conforming_to_reject_rules(token_stream, non_terminal_nodes)
+        non_terminal_nodes.select { |node| node_obeys_reject_rules?(token_stream, node) }
+      end
+
+      def node_obeys_reject_rules?(token_stream, parent_node)
+        if parent_node.non_terminal?
+          derivation_string = token_stream[parent_node.start_index...parent_node.end_index].join()
+          (reject_rules[parent_node.production.non_terminal] || []).none? do |rejection_pattern|
+            if rejection_pattern.is_a? Regexp
+              derivation_string =~ rejection_pattern
+            else
+              derivation_string == rejection_pattern
+            end
+          end
+        else
+          true
+        end
+      end
+
     end     # end TreeValidator
 
     # http://homepages.cwi.nl/~daybuild/daily-books/syntax/2-sdf/sdf.html#section.priorities
@@ -213,28 +193,6 @@ module Linguist
         lesser_production_node.parents << greater_production_node
       end
     
-      # def is_parse_tree_valid?(parent_node)
-      #   if parent_node.non_terminal?
-      #     if @priority_relations.include?(parent_node.production)
-      #       lesser_priority_nodes = @priority_relations[parent_node.production].descendants
-      #       lesser_priority_productions = lesser_priority_nodes.map(&:value)
-      #       # do any of the child nodes cause the Priority rule to be violated?
-      #       is_rule_violated = parent_node.children.any? do |child_node|
-      #         lesser_priority_productions.include?(child_node.production)
-      #       end
-      #       # if is_rule_violated
-      #       #   pp 'rule violated'
-      #       #   # pp parent_node.to_sexp
-      #       # end
-      #       !is_rule_violated
-      #     else
-      #       true    # there are no priority rules that reference parent_node.production
-      #     end
-      #   else
-      #     true      # the parent node represents a terminal symbol, so it is a valid parse tree (i.e. it is a leaf node)
-      #   end
-      # end
-
       def is_parent_children_relationship_valid?(parent_node, child_nodes)
         if parent_node.non_terminal?
           if @priority_relations.include?(parent_node.production)
@@ -294,21 +252,6 @@ module Linguist
         @production = production
       end
     
-      # def is_parse_tree_valid?(parent_node)
-      #   is_parse_tree_invalid = case @direction
-      #     when :left
-      #       # filter out all occurences of P as a direct child of P in the right-most argument
-      #       parent_node.production == @production && parent_node.children.last.production == @production
-      #     when :right
-      #       # filter out all occurences of P as a direct child of P in the left-most argument
-      #       parent_node.production == @production && parent_node.children.first.production == @production
-      #     when :non_assoc
-      #       # filter out all occurrences of P as a direct child of P in any argument
-      #       parent_node.production == @production && parent_node.children.any? {|child_node| child_node.production == @production }
-      #   end
-      #   !is_parse_tree_invalid
-      # end
-
       def is_parent_children_relationship_valid?(parent_node, child_nodes)
         is_parse_tree_invalid = case @direction
           when :left
@@ -331,22 +274,6 @@ module Linguist
         @production_set = production_set
       end
       
-      # def is_parse_tree_valid?(parent_node)
-      #   parent_production = parent_node.production
-      #   is_parse_tree_invalid = @production_set.include?(parent_production) && case @direction
-      #     when :left
-      #       # filter out all occurences of P1 ∈ @production_set that occur as a direct child of P1 ∈ @production_set in the right-most argument
-      #       @production_set.include?(parent_node.children.last.production)
-      #     when :right
-      #       # filter out all occurences of P1 ∈ @production_set that occur as a direct child of P1 ∈ @production_set in the left-most argument
-      #       @production_set.include?(parent_node.children.first.production)
-      #     when :non_assoc
-      #       # filter out all occurences of P1 ∈ @production_set that occur as a direct child of P1 ∈ @production_set in any argument
-      #       parent_node.children.any? {|child_node| @production_set.include?(child_node.production) }
-      #   end
-      #   !is_parse_tree_invalid
-      # end
-
       def is_parent_children_relationship_valid?(parent_node, child_nodes)
         parent_production = parent_node.production
         is_parse_tree_invalid = @production_set.include?(parent_production) && case @direction
--- lib/parse_forest.rb
+++ lib/parse_forest.rb
@@ -137,7 +137,9 @@ module Linguist
     # this is the equivalent of generating a parse forest grammar using the given nodes
     # this is also the equivalent of building a DAG representing the parse forest
     def generate_node_alternatives!
-      select_preferred_and_non_avoided_nodes!    # this enforces any prefer/avoid disambiguation rules
+      @nodes = tree_validator.select_preferred_and_non_avoided_nodes(@nodes)                    # this enforces any prefer/avoid disambiguation rules
+      @nodes = tree_validator.select_nodes_conforming_to_reject_rules(@token_stream, @nodes)    # this enforces any reject disambiguation rules
+      @nodes = tree_validator.select_nodes_obeying_follow_restrictions(@token_stream, @nodes)   # this enforces any follow restrictions (another disambiguation rule)
 
       @nodes.each do |node|
         node.alternatives = generate_alternatives(node)
@@ -148,10 +150,6 @@ module Linguist
       # if any of the root_nodes are one of the nodes that were rejected by the disambiguation rules,
       # then we need to manually remove the root node(s) that were rejected.
       @root_nodes = @root_nodes.select{|root_node| @nodes.include?(root_node) }
-    end
-
-    def select_preferred_and_non_avoided_nodes!
-      @nodes = tree_validator.select_preferred_and_non_avoided_nodes(@nodes)
     end
 
     def filter_node_branches!
@@ -376,14 +374,6 @@ module Linguist
 
         # puts "processing #{node.inspect}"
 
-        # we want to check the subtree for validity before adding this node's children to be processed
-        if branch_is_invalid?(node)
-          # puts 'invalid'
-          # backtrack to the last branch point and select the next possible branch
-          tree_modified = false
-          break
-        end
-
         if node.non_terminal?
           # select the next branch of the current node
           node.select_next_branch!
@@ -398,20 +388,6 @@ module Linguist
       end
 
       [tree_modified, next_node_index, nodes, or_nodes]
-    end
-
-    def branch_is_invalid?(child_node)
-      if child_node.is_rightmost_child?
-        # puts 'checking tree for validity:'
-        # puts child_node.parent.to_sexp
-        valid = tree_validator.nil? || tree_validator.subtree_obeys_disambiguation_rules?(token_stream, child_node.parent)
-        # unless valid
-        #   pp 'invalid'
-        #   pp child_node.parent
-        #   pp child_node
-        # end
-        !valid
-      end
     end
 
     # this method enumerates the trees in the parse forest
